0eff57f5ebc954e25c4bc2f2a9e99c4cae9bc8b6
Ethan M <emohan@luc.edu>
2017-09-25 21:06:23 +0000

    team

--- src/main/java/edu/luc/cs/cs271/lab2/Team.java
+++ src/main/java/edu/luc/cs/cs271/lab2/Team.java
@@ -16,10 +16,18 @@ public class Team {
     if (name == null) {
       throw new IllegalArgumentException("name is null");
     }
-    // TODO validity checking for headcoach
-    // TODO validity checking for funding
+    // done validity checking for headcoach
+    if (headcoach == null) {
+      throw new IllegalArgumentException("headcoach is null");
+    }
+    // done validity checking for funding
+    if (funding <= 0) {
+      throw new IllegalArgumentException("funding is not valid");
+    }
     this.name = name;
-    // TODO complete this constructor
+    // done complete this constructor
+    this.headcoach = headcoach;
+    this.funding = funding;
   }
 
   /** Returns the team's name. */
@@ -29,13 +37,13 @@ public class Team {
 
   /** Returns the team's head coach. */
   public String getHeadcoach() {
-    // TODO complete this method
-    return null;
+    // done complete this method
+    return headcoach;
   }
 
   /** Returns the team's funding level. */
   public int getFunding() {
-    // TODO complete this method
-    return -1;
+    // done complete this method
+    return funding;
   }
 }
