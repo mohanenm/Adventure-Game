4f9df89c4288fdeccd45f1fafc55ee4e2b6a81f9
Eththenet <ethannmohan@gmail.com>
2017-12-03 14:31:37 -0600

    Update roomState.java

--- src/main/java/edu/luc/cs/cs271/lab2/roomState.java
+++ src/main/java/edu/luc/cs/cs271/lab2/roomState.java
@@ -1,35 +1,26 @@
+package main.java.edu.luc.cs.cs271.advent;
+
 import java.util.*;
 
-public class roomState{
-		//LinkedStack<String> stack = new LinkedStack<String>()
-		String[] objectsInScene;
-		Hashmap<playerInput, String> validInputs;
-		
-        roomState(String[] objInScene, Hashmap<playerInput, String> vInputs){
-            objectsInScene = objInScene;
-            validInputs = vInputs;
-        }
-        
-        public getObjects(){
-            return objectsInScene;
-        }
-		
-		public String[] getObjects(){
-		    return objects in Scene
-		   
-		}
-		
-		public void prepOutput(playerInput input){
-		    /*for(playerInput vInput : validInputs.keySet()){
-		        if vInput == input{
-		            System.out.println(validInputs.getValue(vInput));
-		        }
-		    }
-		     *
-		     */
-		}
-       
-      }  
-	}
+public class roomState {
+  //LinkedStack<String> stack = new LinkedStack<String>()
+  String[] objectsInScene;
+  Map<playerInput, String> validInputs;
+
+  roomState(String[] objInScene, Map<playerInput, String> vInputs) {
+    objectsInScene = objInScene;
+    validInputs = vInputs;
+  }
+
+  public String[] getObjects() {
+    return objectsInScene;
+  }
+
+  public void prepOutput(playerInput input) {
+    for (playerInput vInput : validInputs.keySet()) {
+      if (vInput == input) {
+        System.out.println(validInputs.get(vInput));
+      }
+    }
   }
 }
