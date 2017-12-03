59d21262ecb6629b77e470384ba6293f0c00fa8e
Eththenet <ethannmohan@gmail.com>
2017-12-03 14:30:13 -0600

    Update playerInput.java

--- src/main/java/edu/luc/cs/cs271/lab2/playerInput.java
+++ src/main/java/edu/luc/cs/cs271/lab2/playerInput.java
@@ -1,42 +1,46 @@
-public class playerInput{
-    //wrapper for combination of verbs and objects
-    String verb;
-    String objectA;
-    String objectB;
-    
-    playerInput(String predicate){
-        verb = predicate;
+package main.java.edu.luc.cs.cs271.advent;
+//The player input class is a way of containing user inputted data in a way that's readable by other methods.
+//It stores what verb the player is trying to use, as well as up to two objects they're using in conjunction with that action.
+public class playerInput {
+  String verb;
+  String objectA;
+  String objectB;
+
+  public playerInput(String predicate) {
+    verb = predicate;
+  }
+
+  public playerInput(String predicate, String thingA) {
+    verb = predicate;
+    objectA = thingA;
+  }
+
+  public void setObjectA(String thingA) {
+    objectA = thingA;
+  }
+
+  public String getObjectA() {
+    return objectA;
+  }
+
+  public void setObjectB(String thingB) {
+    objectB = thingB;
+  }
+
+  public String getObjectB() {
+    return objectB;
+  }
+
+  public String getVerb() {
+    return verb;
+  }
+
+  public boolean equals(playerInput other) {
+    if (other.verb == verb) {
+      if (other.objectA == objectA) {
+        return other.objectB == objectB;
+      }
     }
-    
-    setObjectA(String thingA){
-        objectA = thingA;
-    }
-    
-    getObjectA(){
-        return objectA;
-    }
-    
-    setObjectB(String thingB){
-        objectB = thingB;
-    }
-    
-    getObjectB(){
-        return objectB;
-    }
-    
-    getVerb(){
-        return verb;
-    }
-    
-    @Override
-    equals(playerInput other){
-        if(other.verb == verb){
-            if(other.objectA == objectA){
-                if(other.objectB = objectB){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+    return false;
+  }
 }
