893465f83d5e41bd54666ec8cc3d6e6241afbf5b
Eththenet <ethannmohan@gmail.com>
2017-12-03 14:38:12 -0600

    Update TestAdventure.java

--- src/test/java/edu/luc/cs/cs271/lab2/TestAdventure.java
+++ src/test/java/edu/luc/cs/cs271/lab2/TestAdventure.java
@@ -1,26 +1,58 @@
-package junit;
+
+
+/* package main.java.edu.luc.cs.cs271.advent;
 
 import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestAdventure{
 
+    String[] verbArr;
+    Map<playerInput, String> inputMap;
+    Stack<roomState> roomStack;
+
 @Before
-public void inputtest(){
-    assertEquals(input!="");
-    assertEquals(/state of room);
+public void setUp(){
+    verbArr = new String[2];
+    verbArr[0] = "Look";
+    verbArr[1] = "Use";
+
+    String[] strArr = new String[2];
+    strArr[0] = "Lever";
+    strArr[1] = "Box";
+    inputMap = new HashMap<playerInput, String>();
+    inputMap.put(new playerInput("Look"), "You see a Room");
+    inputMap.put(new playerInput("Use").setObjectA("Lever"), "You hear the sound of mechnaisms working");
+    roomStack = new Stack<roomState>();
+    roomStack.push(new roomState(objStr, inputMap));
+
+    room fixtureA = new room ("Test Room", "A Room Used for Testing", roomStack);
+    character fixtureB = new character(verbArr, new String[2], fixtureA);
+}
+
+@After
+public void tearDown(){
+    verbArr = null;
+    strArr = null;
+    fixtureA = null;
+    fixtureB = null;
 }
 
 @Test
-public void charactertest(){
-character qwertyuiop;
-assertEquals(getLastInput,playerInput);
+public void testCharacterParser(){
+    fixtureB.parseText("Use Lever");
 
 }
 
 @After
 public void charactertest(){;
-assertEquals(,);
-assertEquals(,);
+//assertEquals(,);
+//assertEquals(,);
 }
 
 }
+
+*/
