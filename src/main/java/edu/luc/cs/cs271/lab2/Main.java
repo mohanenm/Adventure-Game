55333e37c3c03262b902e0e75aa71b753780d529
Eththenet <ethannmohan@gmail.com>
2017-12-03 14:28:48 -0600

    Update Main.java

--- src/main/java/edu/luc/cs/cs271/lab2/Main.java
+++ src/main/java/edu/luc/cs/cs271/lab2/Main.java
@@ -1,9 +1,39 @@
-public Main{
-    public static void main(String args){
-        /*
-        *'wait' for user input
-        *Once user inputs a string send it to the parser (currently 'character.java');
-        */
-    }
-    
+package main.java.edu.luc.cs.cs271.advent;
+
+import java.util.*;
+
+public class Main {
+  public static void Main(String args) {
+    String[] verbArr;
+    Map<playerInput, String> inputMap;
+    Stack<roomState> roomStack;
+
+    verbArr = new String[2];
+    verbArr[0] = "Look";
+    verbArr[1] = "Use";
+
+    String[] strArr = new String[2];
+    strArr[0] = "Lever";
+    strArr[1] = "Box";
+    inputMap = new HashMap<playerInput, String>();
+    inputMap.put(new playerInput("Look"), "You see a Room");
+    inputMap.put(new playerInput("Use"), "You hear the sound of mechnaisms working");
+    roomStack = new Stack<roomState>();
+    roomStack.push(new roomState(strArr, inputMap));
+
+    room fixtureA = new room("Test Room", "A Room Used for Testing", roomStack);
+    character fixtureB = new character(verbArr, new String[2], fixtureA);
+
+    fixtureB.parseText("Use Lever");
+    System.out.print(fixtureB.getLastInput());
+  }
+  /*
+  public static void main(String args){
+      Scanner input = new Scanner(System.in);
+      character player = new character(new String[], new String[], new Room())
+      while(){
+          player.parseText(Scanner.nextLine());
+      }
+  }
+  */
 }
